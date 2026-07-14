d61854c56063ddf1ceb476edc85229689797930a
Yauri Attamimi <yauritux@gmail.com>
2026-07-14 08:54:10 +0700

    Initial commit

---
+++ core/src/main/java/com/catalis/domain/entity/CustomerAccount.java
@@ -0,0 +1,13 @@
+package com.catalis.domain.entity;
+
+import java.math.BigDecimal;
+
+/**
+ * @author yauritux@gmail.com
+ * @version 1.0
+ */
+public class CustomerAccount {
+
+    private String name;
+    private BigDecimal balance;
+}
---
+++ core/src/main/java/com/catalis/sharedkernel/DomainException.java
@@ -0,0 +1,12 @@
+package com.catalis.sharedkernel;
+
+/**
+ * @author yauritux@gmail.com
+ * @version 1.0
+ */
+public class DomainException extends RuntimeException {
+
+    public DomainException(String message) {
+        super(message);
+    }
+}
